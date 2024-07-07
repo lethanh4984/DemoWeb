9639611e404e99f87f6712308a3e42bf8a36b79f
demoweb <lethanh1197@gmail.com>
2024-07-07 16:25:01 +0700

    addmore

--- demo/src/main/java/ASM1/demo/controller/AccountController.java
+++ demo/src/main/java/ASM1/demo/controller/AccountController.java
@@ -1,6 +1,9 @@
 package ASM1.demo.controller;
 
+import ASM1.demo.entity.Donation;
 import ASM1.demo.entity.User;
+import ASM1.demo.entity.UserDonation;
+import ASM1.demo.service.DonationService;
 import ASM1.demo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,5 +16,20 @@ import java.util.List;
 @RequestMapping("/user")
 public class AccountController {
 
+    private DonationService donationService ;
+
+    public AccountController(DonationService donationService) {
+        this.donationService = donationService;
+    }
+
+    @GetMapping("/donations")
+    public String donations(Model theModel){
+
+        List<Donation> donationList = donationService.donations();
+
+        theModel.addAttribute("donationList",donationList);
+
+        return "public/home";
+    }
 
 }
